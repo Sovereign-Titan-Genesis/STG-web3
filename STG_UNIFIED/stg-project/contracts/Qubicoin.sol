865a6168f8fcd8c1326b2f8e76848eae8327a515
Andi Muhammad Harpianto <you@example.com>
2026-05-07 20:32:14 +0000

    cleanup node_modules

---
+++ STG_UNIFIED/stg-project/contracts/Qubicoin.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Qubicoin {
+    string public name = "Qubicoin";
+    string public symbol = "QBC";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor() {
+        totalSupply = 1000000 * 10 ** decimals;
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "Not enough balance");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+}
