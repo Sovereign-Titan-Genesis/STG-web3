9c17c14516491f1d048af1ae7d52915175234634
WWW.QUORUMSTATE.INGERNATIONAL <admin@quorumstate.international>
2026-05-08 00:27:26 +0700

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+// scripts/deploy.js
+async function main() {
+  // Ambil kontrak STGToken dari folder contracts
+  const Token = await ethers.getContractFactory("STGToken");
+
+  console.log("Deploying STGToken...");
+  const token = await Token.deploy();
+
+  await token.waitForDeployment();
+
+  console.log("STGToken deployed to:", await token.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
